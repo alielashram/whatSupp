650bfd786d457b205d16e3affee8b0c706ff747c
Ali El-Ashram <ali.elashram@gmail.com>
2013-08-15 16:20:47 -0700

    Update profile page and add scoped status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ end
   get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
