611e237d1685dc4f8dcdb0f57170b48ddd8bc80c
Ali El-Ashram <ali.elashram@gmail.com>
2013-08-20 01:21:58 -0700

    assets:precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ WhatSupp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
 end
