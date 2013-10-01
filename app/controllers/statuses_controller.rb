22835dc04e4dcef3af1ef9599eb8f4002dabef00
Ali El-Ashram <ali.elashram@gmail.com>
2013-09-30 20:08:46 -0700

    Add destroy to authticate_user! before_filter

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,7 @@
 class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :edit, :update, :destroy]
 
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /statuses
   # GET /statuses.json
@@ -33,10 +33,8 @@ class StatusesController < ApplicationController
     respond_to do |format|
       if @status.save
         format.html { redirect_to @status, notice: 'Status was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @status }
       else
         format.html { render action: 'new' }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -47,10 +45,8 @@ class StatusesController < ApplicationController
     respond_to do |format|
       if @status.update(status_params)
         format.html { redirect_to @status, notice: 'Status was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -61,7 +57,6 @@ class StatusesController < ApplicationController
     @status.destroy
     respond_to do |format|
         format.html { redirect_to statuses_url }
-        format.json { head :no_content }
     end
   end
 
@@ -75,4 +70,6 @@ class StatusesController < ApplicationController
     def status_params
       params.require(:status).permit(:supplication, :user_id)
     end
+
+
 end
